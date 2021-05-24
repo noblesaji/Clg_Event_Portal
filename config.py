2f48cf8b33be41e665cdd46073e1bf7f755d02a9
noblesaji <noblesaji@gmail.com>
2021-05-25 01:24:54 +0530

    mod config1

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or str(os.getenv('SECRET_KEY'))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
