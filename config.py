1728cde661cc58b66cef72992c5a3d7b1d473c76
noblesaji <noblesaji@gmail.com>
2021-05-25 13:58:59 +0530

    adding ql with postgres

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or str(os.getenv('SECRET_KEY'))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
